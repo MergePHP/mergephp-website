629422232e1e4656b786b1e037b44e35fe7c0fb0
Tim Bond <cookieguru@gmail.com>
2024-04-12 20:57:35 -0700

    Fix build check script
    Fix an issue where grep exited with exit code 1 when no lines matched
    the final pattern.  Not matching any lines is the expected behavior, but
    the variable should be populated with empty string instead of causing
    the shell to exit (because of `set -e`).
    
    In addition, do not install dev dependencies since they are not needed

--- .github/workflows/build-check.sh
+++ .github/workflows/build-check.sh
@@ -17,10 +17,10 @@ changes=$(
 	grep -v "^ M docs/sitemap\.xml$" |
 	# the following two are specific to GitHub Actions
 	grep -v "^.. output\.log$" |
-	grep -vi "^.. Docker"
+	grep -vi "^.. Docker" || true # force grep to exit 0, even if no lines match
 )
 
-if [[ -n "$changes" ]]; then
+if [ -n "$changes" ]; then
 	echo "\e[31mYou forgot to commit changes to the docs dir!\e[0m"
 	echo 'On your local run \e[33mcomposer build\e[0m then \e[33mgit add\e[0m your changes to the docs dir'
 	echo 'Then \e[33mgit commit --amend\e[0m and \e[33mgit push --force\e[0m'
