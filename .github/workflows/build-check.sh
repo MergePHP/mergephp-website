8fca62e0fc1f4fb1efa35872ea29d378fcb95655
Tim Bond <cookieguru@gmail.com>
2024-04-12 17:38:14 -0700

    Turn on verbose logging for app commands in GitHub Action debug mode

--- .github/workflows/build-check.sh
+++ .github/workflows/build-check.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-composer build
+trap 'ret=$?; echo "$0 failed on line $LINENO"; exit $ret' ERR
+
+if [[ -z "${RUNNER_DEBUG}" ]]; then
+	composer build
+else
+	set -x
+	composer build -vvv
+fi
+
 changes=$(
 	git status --porcelain |
 	# ignore files that change every time the build command runs
@@ -12,7 +20,7 @@ changes=$(
 	grep -vi "^.. Docker"
 )
 
-if [ -n "$changes" ]; then
+if [[ -n "$changes" ]]; then
 	echo "\e[31mYou forgot to commit changes to the docs dir!\e[0m"
 	echo 'On your local run \e[33mcomposer build\e[0m then \e[33mgit add\e[0m your changes to the docs dir'
 	echo 'Then \e[33mgit commit --amend\e[0m and \e[33mgit push --force\e[0m'
