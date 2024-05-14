100d1adb3bde197b07cf37d8af8ef5224f11955a
Tim Bond <cookieguru@gmail.com>
2024-05-13 22:25:52 -0700

    Fix June 2024 signup link

--- src/Meetup/Meetup20240613MergephpLightningTalksIgniteYourPhpKnowledge.php
+++ src/Meetup/Meetup20240613MergephpLightningTalksIgniteYourPhpKnowledge.php
@@ -19,16 +19,16 @@ class Meetup20240613MergephpLightningTalksIgniteYourPhpKnowledge extends Abstrac
 	{
 		return 'This month, we\'re trying something new: lightning talks! If you have an interesting PHP-related ' .
 			'topic you\'d like to share with the group in a 5-10-minute presentation, now\'s your chance. ' .
-			'[Fill out the sign-up form]([https://mphp.io/june24](https://mphp.io/june24)), and we\'ll email you a ' .
-			'link to join the event 15-20 minutes beforehand. We\'ll bring up each speaker one at a time to give ' .
-			'their talk. Remember, we value contributions from everyone in the community, so don\'t hesitate to ' .
-			"share your knowledge!\n\nHere are some ideas:\n* What's new with JavaScript\n* Alternative PHP run " .
-			"times\n* An update on PHP internals\n* Security\n* New testing strategies\n* Something related to APIs\n" .
-			"* That side project you've been putting off\n\nDepending on the number of lightning talks, we also " .
-			'plan to have a panel discussion. This won\'t be your typical panel discussion-we\'ll watch a YouTube ' .
-			'video together and share our reactions. It\'ll be a fun and interactive experience, similar to the ' .
-			'reaction videos you see on YouTube. Don\'t miss out on this opportunity to learn, share, and engage ' .
-			'with the MergePHP community! 🌩️⚡🗣️';
+			'[Fill out the sign-up form](https://mphp.io/june24), and we\'ll email you a link to join the event ' .
+			'15-20 minutes beforehand. We\'ll bring up each speaker one at a time to give their talk. Remember, we ' .
+			"value contributions from everyone in the community, so don't hesitate to share your knowledge!\n\nHere " .
+			"are some ideas:\n* What's new with JavaScript\n* Alternative PHP runtimes\n* An update on PHP " .
+			"internals\n* Security\n* New testing strategies\n* Something related to APIs\n* That side project " .
+			"you've been putting off\n\nDepending on the number of lightning talks, we also plan to have a panel " .
+			'discussion. This won\'t be your typical panel discussion-we\'ll watch a YouTube video together and ' .
+			'share our reactions. It\'ll be a fun and interactive experience, similar to the reaction videos you see ' .
+			'on YouTube. Don\'t miss out on this opportunity to learn, share, and engage with the MergePHP ' .
+			'community! 🌩️⚡🗣️';
 	}
 
 	public function getDateTime(): DateTimeImmutable
