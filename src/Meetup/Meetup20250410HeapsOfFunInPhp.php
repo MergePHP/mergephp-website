609ac326f30e8a81146fbea9d3a3b800822b7b63
Alena Holligan <alena@smartours.com>
2025-04-04 15:29:36 -0700

    fix line length

--- src/Meetup/Meetup20250410HeapsOfFunInPhp.php
+++ src/Meetup/Meetup20250410HeapsOfFunInPhp.php
@@ -17,7 +17,13 @@ class Meetup20250410HeapsOfFunInPhp extends AbstractMeetup
 
 	public function getDescription(): string
 	{
-		return 'Can PHP be performant for advanced computer science and algorithms? Yes, it can! And it\'s a lot easier and more fun than you might expect due to some unique data structures. A data structure is a way to organize data, and using the proper data structure for each job can make a massive difference in performance. This session will introduce some Standard PHP Library (SPL) data structures you may not have used before and where they come in handy. We\'ll also look into tools for testing performance to find bottlenecks and evaluate the best way to address a challenge. Jump into a heap of fun as we explore data structures, performance and algorithms in PHP.';
+		return 'Can PHP be performant for advanced computer science and algorithms? Yes, it can! ' .
+			'And it\'s a lot easier and more fun than you might expect due to some unique data structures. ' .
+			'A data structure is a way to organize data, and using the proper data structure for each job can ' .
+			'make a massive difference in performance. This session will introduce some Standard PHP Library (SPL) ' .
+			'data structures you may not have used before and where they come in handy. We\'ll also look into tools ' .
+			'for testing performance to find bottlenecks and evaluate the best way to address a challenge. ' .
+			'Jump into a heap of fun as we explore data structures, performance and algorithms in PHP.';
 	}
 
 	public function getDateTime(): DateTimeImmutable
@@ -38,7 +44,10 @@ class Meetup20250410HeapsOfFunInPhp extends AbstractMeetup
 
 	public function getSpeakerBio(): string
 	{
-		return 'For over 20 years, Alena has built technical solutions that inform decisions and solve problems across diverse industries. She enjoys both the creativity of programming and the thrill of solving a puzzle. As a leader in the community, a technical trainer, and a mom, she is passionate about providing the tools and mindset required for everyone to learn and succeed.';
+		return 'For over 20 years, Alena has built technical solutions that inform decisions and solve problems ' .
+			'across diverse industries. She enjoys both the creativity of programming and the thrill of solving ' .
+			'a puzzle. As a leader in the community, a technical trainer, and a mom, she is passionate about ' .
+			'providing the tools and mindset required for everyone to learn and succeed.';
 	}
 
 	public function getYouTubeLink(): ?string
