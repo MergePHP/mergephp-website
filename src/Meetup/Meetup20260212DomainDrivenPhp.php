e4afceee3083b801ae0d48d7a53153b1f824fd5b
Eric Poe <eric.poe@gmail.com>
2026-01-06 14:11:27 -0600

    Fix line length

--- src/Meetup/Meetup20260212DomainDrivenPhp.php
+++ src/Meetup/Meetup20260212DomainDrivenPhp.php
@@ -23,11 +23,21 @@ class Meetup20260212DomainDrivenPhp extends AbstractMeetup
 	public function getDescription(): string
 	{
 		return <<<END
-		In the modern PHP ecosystem, we often let frameworks dictate our application's architecture. We start with composer create-project and immediately begin molding our business logic to fit the constraints of a specific "Way." But what happens when the framework becomes a hurdle rather than a helper?
-		
-		This session explores the Art of Domain-Driven Design (DDD) by stripping away the safety net of frameworks. We will start from a "naked" PHP environment, demonstrating how to build a robust, testable, and scalable application using only pure PHP and the principles of DDD. By focusing on Entities, Value Objects, Aggregates, and Domain Services without the noise of an ORM or a heavy container, you will learn to see your business logic as the primary citizen of your codebase.
-		
-		Once we have established the "Pure Domain" foundation, we will pivot to the pragmatic reality of modern development: Integration. We will discuss how to take these framework-agnostic principles and safely "plug" them into popular frameworks like Laravel or Symfony. You’ll learn how to treat the framework as a replaceable infrastructure detail—leveraging its power for routing and delivery while keeping your core logic untainted and portable.
+		In the modern PHP ecosystem, we often let frameworks dictate our application's architecture. We start with
+		composer create-project and immediately begin molding our business logic to fit the constraints of a specific
+		"Way." But what happens when the framework becomes a hurdle rather than a helper?
+
+		This session explores the Art of Domain-Driven Design (DDD) by stripping away the safety net of frameworks.
+		We will start from a "naked" PHP environment, demonstrating how to build a robust, testable, and scalable
+		application using only pure PHP and the principles of DDD. By focusing on Entities, Value Objects, Aggregates,
+		and Domain Services without the noise of an ORM or a heavy container, you will learn to see your business
+		logic as the primary citizen of your codebase.
+
+		Once we have established the "Pure Domain" foundation, we will pivot to the pragmatic reality of modern
+		development: Integration. We will discuss how to take these framework-agnostic principles and safely "plug"
+		them into popular frameworks like Laravel or Symfony. You'll learn how to treat the framework as a replaceable
+		infrastructure detail—leveraging its power for routing and delivery while keeping your core logic untainted
+		and portable.
 		END;
 	}
 
