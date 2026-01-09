62f1c73df24346795107595ad7a2bd971ecab5e8
Tim Bond <cookieguru@gmail.com>
2026-01-08 18:19:35 -0800

    Add GitHub link for code for January 2026 meetup
    Added link to GitHub repository for code from slides.

--- src/Meetup/Meetup20260108MasteringAgenticPhpDevelopmentWithMcp.php
+++ src/Meetup/Meetup20260108MasteringAgenticPhpDevelopmentWithMcp.php
@@ -24,6 +24,8 @@ class Meetup20260108MasteringAgenticPhpDevelopmentWithMcp extends AbstractMeetup
 		that connects directly to PHP.net documentation. We'll also implement Composer package search for intelligent
 		dependency discovery. Join me to see how this Master Control Program...unlike the TRON original...is actually
 		here to help.
+
+		Code from the slides is available at [https://github.com/hskrasek/php-mcp](https://github.com/hskrasek/php-mcp)
 		END;
 	}
 
